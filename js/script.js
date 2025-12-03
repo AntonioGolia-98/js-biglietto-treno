92b911d36f5497a4149ab3ffebc87643bd63340d
AntonioGolia-98 <antoniog98@icloud.com>
2025-12-03 18:57:09 +0100

    aggiunta tofixed

--- js/script.js
+++ js/script.js
@@ -1,20 +1,19 @@
 
-const km = parseInt(prompt("Inserisci il numero di km che andari ad effettuare"));
+const km = parseInt(prompt("Inserisci il numero di km che andrai ad effettuare"));
 const età = parseInt(prompt("Inserisci la tua età"));
 
 const ticketKm = 0.21;
 const ticket= km * ticketKm;
 const scontoMinorenni= (ticket * 20) /100;
 const scontoOver= (ticket * 40) / 100;
+let ticketMinorenni= ticket - scontoMinorenni;
+let ticketOver= ticket - scontoOver;
 
 
 if (età > 18 && età < 65) {
     console.log(`il prezzo del biglietto è ${ticket}`);
 }else if (età < 18){
-    console.log(`il prezzo del biglietto è ${ticket - scontoMinorenni}`);
+    console.log(`il prezzo del biglietto è ${ticketMinorenni.toFixed(2)}`);
 }else if (età > 65 ){
-    console.log(`il prezzo del biglietto è ${ticket - scontoOver}`);
+    console.log(`il prezzo del biglietto è ${ticketOver.toFixed(2)}`);
 }
-
-
-
